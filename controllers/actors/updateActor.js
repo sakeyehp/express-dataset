d9e35ba7319b9426522d71fed79f38ced18a0e53
sakeyehp <sakeyehp@yahoo.co.uk>
2020-02-26 04:36:47 +0100

    add logic for updateActor

--- controllers/actors/updateActor.js
+++ controllers/actors/updateActor.js
@@ -1,6 +1,22 @@
 const Actor = require('../../models/Actor');
 
 const updateActor = async (req, res) => {
+    const { avatar_url, id, login } = req.body;
+  const actorInDB = await Actor.findByPrimary(id);
+  if (!actorInDB) {
+    return res.status(404).send({});
+  }
+  if (login !== actorInDB.login) {
+    return res.status(400).send({});
+  }
+  await Actor.update({
+    avatar_url,
+  }, {
+    where: {
+      id,
+    },
+  });
+  return res.status(200).send({});
 };
 
 module.exports = updateActor;
